76b6771f11f23ff648c3c7d886c30deecfd8334d
icemandb3 <don@lloydcenterice.com>
2013-10-04 15:58:39 -0700

    Latest Changes

--- mysite/js/_vti_cnf/skatelessons.js
+++ mysite/js/_vti_cnf/skatelessons.js
@@ -5,7 +5,7 @@ vti_modifiedby:SR|Don-PC\\Don
 vti_nexttolasttimemodified:TW|26 Aug 2013 00:55:55 -0000
 vti_timecreated:TR|15 Jun 2012 22:39:23 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|testcart/default.html SkatingLessons/schedule.html testcart/default1.html SkatingLessons/sched.html SkatingLessons/sched2.html SkatingLessons/scheduleTestDDon.html
+vti_backlinkinfo:VX|testcart/default.html SkatingLessons/scheduleARCHIVE.html testcart/default1.html SkatingLessons/sched.html SkatingLessons/sched2.html SkatingLessons/scheduleTestDDon.html
 vti_syncwith_www.lloydcenterice.biz\:21/public_html:TW|15 Jun 2012 22:39:23 -0000
 vti_syncofs_www.lloydcenterice.biz\:21/public_html:TW|18 May 2012 19:54:00 -0000
 vti_syncofs_ftp.lloydcenterice.info\:21:TW|26 Aug 2013 00:55:55 -0000
